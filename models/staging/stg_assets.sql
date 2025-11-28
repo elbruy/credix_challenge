00e1a3f54e1268e5023ce843e0b56dced8399a3a
Breno Ruy <ruybr11@gmail.com>
2025-11-28 15:02:39 +0000

    fixes fixes fixes

--- models/staging/stg_assets.sql
+++ models/staging/stg_assets.sql
@@ -4,14 +4,9 @@ with
     -- remove exact duplicates that come from new raw file
     deduped as (select distinct * from source),
 
-    -- filter out 0 value artifacts once they are not real loans
-    filtered as (select * from deduped where face_value > 0),
-
     explicit_cast_and_rename as (
-
         select
-            -- asset_id is missing! 
-            -- creates a hash of unique-ish columns to serve as the ID
+            -- surrogate key creation
             to_hex(
                 md5(
                     concat(
@@ -21,21 +16,19 @@ with
                     )
                 )
             ) as asset_id,
-            -- protects leading zeros 
             cast(buyer_tax_id as string) as buyer_tax_id,
             collection_status,
             seller_name,
             buyer_state,
-            -- no floating point error
             cast(face_value as numeric) as face_value,
-            -- enforces timestamp
             cast(created_at as timestamp) as created_at,
             cast(settled_at as timestamp) as settled_at,
             cast(due_date as date) as due_date
 
-        from filtered
-
+        from deduped
     )
 
 select *
 from explicit_cast_and_rename
+-- remove invalid loans
+where face_value > 0
