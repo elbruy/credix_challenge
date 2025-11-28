6880a8b2fb2db8c98ab1455e19b54f4299976956
Breno Ruy <ruybr11@gmail.com>
2025-11-28 15:56:14 +0000

    align status with reality

--- models/marts/fct_credit_risk.sql
+++ models/marts/fct_credit_risk.sql
@@ -11,52 +11,63 @@ with
             a.due_date,
             a.settled_at,
             a.buyer_state,
-            case
-                when a.collection_status in ('Settled', 'Repaid', 'Paid')
-                then 'Settled'
-                when a.collection_status in ('Default', 'Defaulted')
-                then 'Defaulted'
-                when a.collection_status = 'Canceled'
-                then 'Canceled'
-                else 'Active'
-            end as collection_status,
-            r.current_rating
+            r.current_rating,
+            date_diff(current_date, a.due_date, day) as days_overdue,
+            a.collection_status as raw_status
         from assets a
         inner join ratings r on a.buyer_tax_id = r.buyer_tax_id
     ),
 
-    with_provision_rate as (
+    with_status_logic as (
         select
             *,
-            date_diff(current_date, due_date, day) as days_overdue,
+            -- status based on business rule + data
             case
-                -- no risk
+                -- hard coded statuses
+                when raw_status in ('Settled', 'Repaid', 'Paid')
+                then 'Settled'
+                when raw_status = 'Canceled'
+                then 'Canceled'
+
+                -- business logic default (overdue)
+                when raw_status in ('Default', 'Defaulted') or days_overdue > 30
+                then 'Defaulted'
+
+                -- else is active
+                else 'Active'
+            end as collection_status
+        from joined
+    ),
+
+    final_metrics as (
+        select
+            *,
+            case
+                when collection_status = 'Defaulted'
+                then 1.0
                 when collection_status in ('Settled', 'Canceled')
                 then 0.0
-
-                -- defaulted already
-                when
-                    collection_status = 'Defaulted'
-                    or date_diff(current_date, due_date, day) > 30
-                then 1.0
-
-                -- active
-                when current_rating = 'A'
-                then 0.01
-                when current_rating = 'B'
-                then 0.05
-                when current_rating = 'C'
-                then 0.10
-                when current_rating = 'D'
-                then 0.20
-                when current_rating = 'E'
-                then 0.30
-                when current_rating = 'F'
-                then 0.40
+                when collection_status = 'Active'
+                then
+                    case
+                        when current_rating = 'A'
+                        then 0.01
+                        when current_rating = 'B'
+                        then 0.05
+                        when current_rating = 'C'
+                        then 0.10
+                        when current_rating = 'D'
+                        then 0.20
+                        when current_rating = 'E'
+                        then 0.30
+                        when current_rating = 'F'
+                        then 0.40
+                        else 0.0
+                    end
                 else 0.0
             end as provision_rate
-        from joined
+        from with_status_logic
     )
 
 select *, (face_value * provision_rate) as cost_of_risk_amount
-from with_provision_rate
+from final_metrics
