4d0093b334489ce267f57694542b48620b3bebfa
Breno Ruy <ruybr11@gmail.com>
2025-11-28 15:21:41 +0000

    better surrogate key

--- models/staging/stg_assets.sql
+++ models/staging/stg_assets.sql
@@ -1,9 +1,6 @@
 with
     source as (select * from {{ source("raw_data", "assets") }}),
 
-    -- remove exact duplicates that come from new raw file
-    deduped as (select distinct * from source),
-
     explicit_cast_and_rename as (
         select
             -- surrogate key creation
@@ -12,7 +9,9 @@ with
                     concat(
                         cast(created_at as string),
                         cast(buyer_tax_id as string),
-                        cast(face_value as string)
+                        cast(face_value as string),
+                        cast(settled_at as string),
+                        cast(buyer_state as string)
                     )
                 )
             ) as asset_id,
@@ -25,7 +24,7 @@ with
             cast(settled_at as timestamp) as settled_at,
             cast(due_date as date) as due_date
 
-        from deduped
+        from source
     )
 
 select *
